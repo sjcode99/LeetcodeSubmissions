4b9411683aa41889b4a685bb503d2165e96abce8
sjcode99 <52974372+sjcode99@users.noreply.github.com>
2022-12-03 13:50:56 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -30,33 +30,34 @@ public:
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
         vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<char>> newMat = mat;
         
         int delrow[] = {-1,0,+1,0};
         int delcol[] = {0,+1,0,-1};
         
         for(int j=0; j<m; j++){
             if(!vis[0][j] && mat[0][j] == 'O')
-                dfs(0, j, vis, mat, delrow, delcol);
+                dfs(0, j, vis, newMat, delrow, delcol);
                 
             if(!vis[n-1][j] && mat[n-1][j] == 'O')
-                dfs(n-1, j, vis, mat, delrow, delcol);
+                dfs(n-1, j, vis, newMat, delrow, delcol);
         }
         
         for(int i=0; i<n; i++){
             if(!vis[i][0] && mat[i][0] == 'O')
-                dfs(i, 0, vis, mat, delrow, delcol);
+                dfs(i, 0, vis, newMat, delrow, delcol);
                 
             if(!vis[i][m-1] && mat[i][m-1] == 'O')
-                dfs(i, m-1, vis, mat, delrow, delcol);
+                dfs(i, m-1, vis, newMat, delrow, delcol);
         }
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!vis[i][j] && mat[i][j] == 'O')
-                    mat[i][j] = 'X';
+                if(!vis[i][j] && newMat[i][j] == 'O')
+                    newMat[i][j] = 'X';
             }
         }
-        return mat;
+        return newMat;
     }
 };
 
