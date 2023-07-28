327d658f9e0ccbc383dbd1ce99025ce98a8d95b5
sjcode99 <52974372+sjcode99@users.noreply.github.com>
2023-07-28 13:42:15 +0530

    Time: 75 ms (98.18%), Space: 58.7 MB (9.56%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+     const map = new Map();
+        
+    for(const num of nums){
+        if(map.has(num)) return true;
+        map.set(num, true);
+    }
+    return false;
+};
