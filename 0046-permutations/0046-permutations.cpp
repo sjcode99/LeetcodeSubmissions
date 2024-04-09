2615ddc7aeee2faaf84fee3db94b1f5027066e02
Suraj lal <52974372+sjcode99@users.noreply.github.com>
2024-04-09 18:37:45 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (88.06%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,29 @@
 class Solution {
 private:
-    void solve(vector<int>&nums, vector<vector<int>>& ans, int idx){
-        
-        if(idx >= nums.size()){
-            ans.push_back(nums);
+    void helper(vector<int>& nums, int freq[], vector<vector<int>>& ans, vector<int>& arr, int n){
+        if(arr.size() == nums.size()){
+            ans.push_back(arr);
             return;
         }
         
-        for(int j=idx; j<nums.size(); j++){
-            swap(nums[idx], nums[j]);
-            solve(nums, ans, idx+1);
-            swap(nums[idx], nums[j]);
+        for(int i=0; i<n; i++){
+            if(!freq[i]){
+                arr.push_back(nums[i]);
+                freq[i] = 1;
+                helper(nums, freq, ans, arr, n);
+                freq[i] = 0;
+                arr.pop_back();
+            }
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int idx = 0;
-        solve(nums, ans, idx);
+        vector<int> temp;
+        int n = nums.size();
+        int freq[n];
+        for(int i=0; i<n; i++) freq[i] = 0;
+        helper(nums, freq, ans, temp, n);
         
         return ans;
     }
