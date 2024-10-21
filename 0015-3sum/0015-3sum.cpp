2c23c91b8006939c74360026d982769bdfc339e5
Suraj lal <52974372+sjcode99@users.noreply.github.com>
2024-10-21 16:16:35 +0530

    Time: 55 ms (84.85%), Space: 29.3 MB (43.27%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int> > res;
+        int n = nums.size();
         
-        for(int i=0; i<n-1; i++){
-            if(i == 0 || (i<n && nums[i] != nums[i-1])){
-                int lt=i+1, rt=n-1, sum = 0-nums[i];
-                
-                while(lt < rt){
-                    if(nums[lt] + nums[rt] == sum){
-                        vector<int> temp;
-                        temp.push_back(nums[lt]);
-                        temp.push_back(nums[rt]);
-                        temp.push_back(nums[i]);
-                        res.push_back(temp);
-                        
-                        while(lt < rt && nums[lt] == nums[lt+1]) lt++;
-                        while(lt < rt && nums[rt] == nums[rt-1]) rt--;
-                        
-                        lt++;
-                        rt--;
-                    }
-                    else if(nums[lt] + nums[rt] < sum) lt++;
-                    else rt--;
-                }
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    vector<int> tmp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(tmp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
+                }   
             }
         }
-        return res;
+        return ans;
     }
 };
