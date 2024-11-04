d3ea37033d41b5da10ec56be75c05867e16bf318
Suraj lal <52974372+sjcode99@users.noreply.github.com>
2024-11-04 13:57:04 +0530

    Time: 91 ms (9.98%), Space: 35.2 MB (8.33%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    void solve(vector<int>& nums, vector<vector<int>>& ans, int n, int index){
+        //base case
+        if(index >= n){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=index; i<n; i++){
+            swap(nums[i], nums[index]);
+            solve(nums, ans, n, index+1);
+            //backtrack
+            swap(nums[i], nums[index]);
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int index = 0;
+        
+        solve(nums, ans, n, index);
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return ans;
+    }
+};
